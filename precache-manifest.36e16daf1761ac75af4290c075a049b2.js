f4efe7b2d28eee3652e4e8f730a23132f3d21854
manan2501 <kevadiyamanan@gmail.com>
2020-01-11 10:02:06 +0530

    Updates

---
+++ precache-manifest.36e16daf1761ac75af4290c075a049b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc32216519e63e693cd916354e98d33",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "7bbb07ab70997a3641db",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "be13f52b32eb9ac22abc",
+    "url": "/github-finder/static/js/2.59fed4b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-finder/static/js/2.59fed4b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bbb07ab70997a3641db",
+    "url": "/github-finder/static/js/main.9525734d.chunk.js"
+  },
+  {
+    "revision": "84e217357f4684757524",
+    "url": "/github-finder/static/js/runtime-main.6348006c.js"
+  }
+]);
